a686582909951a1383860059c5d6132c8e7b1977
simon <simonkhartoum@gmail.com>
2023-05-30 09:39:00 +0300

    completed

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age,sex,color,temperament, alive) VALUES ("Mr. Chocolate", 3, "M", "Brown", "Neutral", True);
+INSERT INTO bears (name, age,sex,color,temperament, alive) VALUES ("Rowdy", 4, "M", "Green", "Neutral", True);
+INSERT INTO bears (name, age,sex,color,temperament, alive) VALUES ("Tabitha", 1, "M", "Orange", "High", True);
+INSERT INTO bears (name, age,sex,color,temperament, alive) VALUES ("Sergeant Brown", 5, "F", "Red", "Low", True);
+INSERT INTO bears (name, age,sex,color,temperament, alive) VALUES ("Melissa", 8, "M", "Purple", "Moderate", False);
+INSERT INTO bears (name, age,sex,color,temperament, alive) VALUES ("Grinch", 3, "M", "Blue", "Low", True);
+INSERT INTO bears (name, age,sex,color,temperament, alive) VALUES ("Wendy", 7, "F", "White", "Neutral", True);
+INSERT INTO bears (name, age,sex,color,temperament, alive) VALUES (null, 1, "M", "Black", "High", False);
